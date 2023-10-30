356bab64df6bae487d84fb8a274ac75ae951eae6
wkdwns0501 <britain0501@naver.com>
2023-10-30 16:34:58 +0900

    리액트 첫 번째 상태변수 생성

---
+++ demo03/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Exam01 from './components/Exam01';
+
+function App() {
+  return (
+    <>
+    <h1>각종 리액트 예제</h1>
+    <hr></hr>
+    <Exam01/>
+    </>
+  );
+}
+
+export default App;
