dbb67b6d808255319c942ccc1fb059bd0375dc02
wkdwns0501 <britain0501@naver.com>
2023-11-07 16:11:01 +0900

    더보기 버튼으로 무한 스크롤을 구현

--- demo05/src/components/BookInfinite.js
+++ demo05/src/components/BookInfinite.js
@@ -8,7 +8,7 @@ const BookInfinite = (props)=>{
 
     const loadBook = ()=>{
         axios({
-            url:`process.env.REACT_APP_REST_API_URL/book/page/${page}/size/${size}`,
+            url:`${process.env.REACT_APP_REST_API_URL}/book/page/${page}/size/${size}`,
             method:"get"
         })
         .then(response=>{
@@ -17,15 +17,39 @@ const BookInfinite = (props)=>{
             // setBookList(bookList.concat(...response.data));//concat 함수
         })
     };
+
     useEffect(()=>{
         loadBook();
-    }, []);
+    }, [page]);
+
+    //다음 페이지
+    const nextPage= ()=>{
+        setPage(page+1);//페이지 1 증가
+    };
+
+    //개수가 변하면 페이지를 1로, 목록을 모두 지우고 다시 불러와야 한다
+    useEffect(()=>{
+        setPage(1);
+        setBookList([]);
+        //loadBook();
+    }, [size])
 
     return (
         <>
         <div className="row">
             <div className="col">
                 <h1 className="mt-4">무한 스크롤 예제</h1>
+            </div>
+        </div>
+
+        <div className="row mt-4">
+            <div className="col-2 offset-10">
+                <select value={size} onChange={e=>setSize(e.target.value)}>
+                    <option value="20">20개씩 보기</option>
+                    <option value="30">30개씩 보기</option>
+                    <option value="50">50개씩 보기</option>
+                    <option value="100">100개씩 보기</option>
+                </select>
             </div>
         </div>
 
@@ -50,6 +74,16 @@ const BookInfinite = (props)=>{
                     </tbody>
                 </table>
             </div>
+
+                {/* 더보기 버튼 */}
+                <div className="row mt-2">
+                    <div className="col">
+                        <button className="btn btn-primary w-100" onClick={nextPage}>
+                            {size} 개 더보기
+                        </button>
+                    </div>
+                </div>
+
         </div>
         </>
     );
